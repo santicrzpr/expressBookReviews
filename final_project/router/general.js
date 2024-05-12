6631a736ca04e67ed307a0c8a976bcfbabc0a45c
santicrzpr <167240731+santicrzpr@users.noreply.github.com>
2024-05-12 15:56:23 -0600

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -81,7 +81,14 @@ public_users.get('/title/:title',function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  if (books.hasOwnProperty(isbn)) {
+    const bookReviews = books[isbn].reviews;
+    return res.status(200).json(bookReviews);
+  } else {
+    return res.status(404).json({ error: "Book not found" });
+  }  
+//  return res.status(300).json({message: "Yet to be implemented"});
 });
 
 module.exports.general = public_users;
